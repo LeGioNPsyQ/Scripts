c773b2105c45623de2331f44a4bc93306ec6940b
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-01-31 12:51:47 +0100

    Update Script Hub - DevilNetWork X.lua

--- Script Hub - DevilNetWork X.lua
+++ Script Hub - DevilNetWork X.lua
@@ -8,14 +8,14 @@ local Games = {
 
 if Games[game.PlaceId] then
 	game.StarterGui:SetCore("SendNotification", {
-	    Title = "👽 LeGioN Hub X Notification";
+	    Title = "[LeGioN Hub] Notification";
 	    Text = "Script is loaded";
 	    Duration = 10;
     })
 	loadstring(game:HttpGet(Games[game.PlaceId]))()
 else
 	game.StarterGui:SetCore("SendNotification", {
-	    Title = "👽 LeGioN Hub X Notify";
+	    Title = "[LeGioN Hub] Notify";
 	    Text = "Game not supported";
 	    Duration = 10;
     })
